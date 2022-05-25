9cd2294d96a40d75907c76cbfb9e29511ebd5b10
atxoupi <andres.hermelo@gmail.com>
2022-05-25 19:53:57 +0000

    Falta anclar el footer al pies de la página

---
+++ src/js/component/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<>
+			<footer className="bg-dark text-white text-center text-lg-start d-flex justify-content-evenly">
+				<div className="text-center p-3">© 2021 Copyright:</div>
+			</footer>
+		</>
+	);
+};
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import Navbar from "./Navbar.js";
+import Jumbotron from "./Jumbotron.js";
+import Footer from "./Footer.js";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,20 +9,13 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="flex-column vh-100">
+				<Navbar />
+				<Jumbotron />
+				<Footer />
+			</div>
+		</>
 	);
 };
 
